277cf5b5773abbc4a9603f6f8e13c88ecc2fdfae
John Doe <stanislav_2010@icloud.com>
2021-11-21 17:12:09 +0500

    Add starter tempalate in project

---
+++ dist/JS/bundle.js
@@ -0,0 +1,12 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*************************!*\
+  !*** ./src/JS/index.js ***!
+  \*************************/
+
+
+document.addEventListener('DOMContentLoaded', function () {});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
